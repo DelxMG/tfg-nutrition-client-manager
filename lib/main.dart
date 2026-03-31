b1f841379f2074e4df48b3bb0a47a5ba15e5e87d
DelxMG <deliamoya.g@gmail.com>
2026-03-31 12:18:08 +0200

    chore: initial flutter project setup

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NutriTrackApp());
+}
+
+class NutriTrackApp extends StatelessWidget {
+  const NutriTrackApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NutriTrack',
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
+        body: Center(
+          child: Text('NutriTrack'),
+        ),
+      ),
+    );
+  }
+}
