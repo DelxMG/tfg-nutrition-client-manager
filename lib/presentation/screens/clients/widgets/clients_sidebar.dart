5cc82e59af2bb36e19ba10c38b7ee3a21a393e0f
DelxMG <deliamoya.g@gmail.com>
2026-04-25 19:22:04 +0200

    fix: resolve merge conflicts in client detail header

--- lib/presentation/screens/clients/widgets/clients_sidebar.dart
+++ lib/presentation/screens/clients/widgets/clients_sidebar.dart
@@ -1,5 +1,7 @@
 // Redirected to the global layout widget.
 // Keep this file so existing imports don't break.
+import 'package:nutritrack/presentation/layout/app_sidebar.dart';
+
 export 'package:nutritrack/presentation/layout/app_sidebar.dart'
     show AppSidebar;
 
--- lib/presentation/screens/clients/widgets/clients_top_bar.dart
+++ lib/presentation/screens/clients/widgets/clients_top_bar.dart
@@ -1,5 +1,7 @@
 // Redirected to the global layout widget.
 // Keep this file so existing imports don't break.
+import 'package:nutritrack/presentation/layout/app_top_bar.dart';
+
 export 'package:nutritrack/presentation/layout/app_top_bar.dart'
     show AppTopBar;
 
