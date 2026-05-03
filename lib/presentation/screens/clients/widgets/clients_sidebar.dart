26afa19a92dac80538d61d6a52850c5778cf12eb
DelxMG <deliamoya.g@gmail.com>
2026-05-03 22:01:46 +0200

    chore: remove unused scaffolding files

--- lib/presentation/screens/clients/widgets/clients_sidebar.dart
+++
@@ -1,9 +0,0 @@
-// Redirected to the global layout widget.
-// Keep this file so existing imports don't break.
-import 'package:nutritrack/presentation/layout/app_sidebar.dart';
-
-export 'package:nutritrack/presentation/layout/app_sidebar.dart'
-    show AppSidebar;
-
-/// Alias kept for backwards compatibility.
-typedef ClientsSidebar = AppSidebar;
--- lib/presentation/screens/clients/widgets/clients_top_bar.dart
+++
@@ -1,9 +0,0 @@
-// Redirected to the global layout widget.
-// Keep this file so existing imports don't break.
-import 'package:nutritrack/presentation/layout/app_top_bar.dart';
-
-export 'package:nutritrack/presentation/layout/app_top_bar.dart'
-    show AppTopBar;
-
-/// Alias kept for backwards compatibility.
-typedef ClientsTopBar = AppTopBar;
